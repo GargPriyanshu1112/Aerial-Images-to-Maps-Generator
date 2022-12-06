443511aa20f029cd112cc9749937bddba33c9c3f
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2022-12-06 19:26:09 +0530

    Update helper_functions.py

--- Files/helper_functions.py
+++ Files/helper_functions.py
@@ -1,4 +1,4 @@
-def summarize_performance(epoch, g_model, dataset, n_samples=3): ######### use of step
+def summarize_performance(epoch, g_model, dataset, n_samples=3):
     [X1_real, X2_real], _ = generate_real_samples(dataset, n_samples, 1)
     X2_fake, _ = generate_fake_samples(g_model, n_samples, 1)
 
@@ -7,6 +7,7 @@ def summarize_performance(epoch, g_model, dataset, n_samples=3): ######### use o
     X2_real = np.uint8((X1_real + 1) * 127.5)
     X2_fake = np.uint8((X1_real + 1) * 127.5)
 
+    # Plot the images
     for i in range(n_samples):
         plt.subplot(n_samples, 3, i+1)
         plt.title("Real Source Image")
@@ -22,8 +23,18 @@ def summarize_performance(epoch, g_model, dataset, n_samples=3): ######### use o
         plt.title("Generated Target Image")
         plt.imshow(X2_fake[i])
         plt.axis(False)
-
     plt.show()
+
+    # Save the generator model
+    fname1 = f"model_{epoch+1}.h5"
+    g_model.save(fname1)
+
+    # Save plot as .png file
+    fname2 = f"plot_{epoch+1}.png"
+    plt.savefig(fname2)
+
+    print(f"Saving Model: {fname1} and Plot: {fname2}...")
+    plt.close()
 
     
 def plot_images(src_img, gen_img, tar_img):
