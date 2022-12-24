92902258870a80c9a7cd4b80d4f1f946e486a6a8
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2022-12-24 14:31:04 +0530

    Update and rename helper_functions.py to utilities.py

--- Files/utilities.py
+++ Files/utilities.py
@@ -1,26 +1,27 @@
 def summarize_performance(epoch, g_model, dataset, n_samples=3):
     [X1_real, X2_real], _ = generate_real_samples(dataset, n_samples, 1)
-    X2_fake, _ = generate_fake_samples(g_model, n_samples, 1)
+    X2_fake, _ = generate_fake_samples(g_model, X1_real, 1)
 
     # Rescale the images
     X1_real = np.uint8((X1_real + 1) * 127.5)
-    X2_real = np.uint8((X1_real + 1) * 127.5)
-    X2_fake = np.uint8((X1_real + 1) * 127.5)
+    X2_real = np.uint8((X2_real + 1) * 127.5)
+    X2_fake = np.uint8((X2_fake + 1) * 127.5)
 
+    print("    Source Images\t\t\t\t  Target Images\t\t\t\tGenerated Image")
+    plt.figure(figsize=(12, 9))
     # Plot the images
     for i in range(n_samples):
-        plt.subplot(n_samples, 3, i+1)
-        plt.title("Real Source Image")
+        plt.subplot(n_samples, 3, 3*i+1)
         plt.imshow(X1_real[i])
         plt.axis(False)
-
-        plt.subplot(n_samples, 3, i+1)
-        plt.title("Real Target Image")
+    
+    for i in range(n_samples):
+        plt.subplot(n_samples, 3, 3*i+2)
         plt.imshow(X2_real[i])
         plt.axis(False)
-
-        plt.subplot(n_samples, 3, i+1)
-        plt.title("Generated Target Image")
+    
+    for i in range(n_samples):
+        plt.subplot(n_samples, 3, 3*i+3)
         plt.imshow(X2_fake[i])
         plt.axis(False)
     plt.show()
@@ -33,10 +34,10 @@ def summarize_performance(epoch, g_model, dataset, n_samples=3):
     fname2 = f"plot_{epoch+1}.png"
     plt.savefig(fname2)
 
-    print(f"Saving Model: {fname1} and Plot: {fname2}...")
+    print(f"Saving Model: {fname1}   |   Saving Plot: {fname2}")
     plt.close()
 
-    
+
 def plot_images(src_img, gen_img, tar_img):
     images = np.vstack([src_img, gen_img, tar_img])
     images = (images + 1) * 127.5 # Rescale the images
