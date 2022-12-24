c3d524d746b26ec3820710bf211e477e149804e1
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2022-12-24 20:18:55 +0530

    Update gan_model.py

--- Files/gan_model.py
+++ Files/gan_model.py
@@ -1,13 +1,8 @@
-# Download the generator model
-!wget https://raw.githubusercontent.com/GargPriyanshu1112/Image-to-Image-Translation/main/Files/generator_model.py
-
-# Download the discriminator model
-!wget https://raw.githubusercontent.com/GargPriyanshu1112/Image-to-Image-Translation/main/Files/discriminator_model.py
-
 # Import dependencies
 import tensorflow as tf
 from tensorflow.keras.layers import BatchNormalization, Input
 from tensorflow.keras.models import Model
+
 from generator_model import define_generator
 from discriminator_model import define_discriminator
 
@@ -23,7 +18,7 @@ def define_gan(g_model, d_model, inp_shape):
         if not isinstance(layer, BatchNormalization):
             layer.trainable = False
     
-    src_img = Input(shape=inp_shape) # source image
+    src_img = Input(shape=inp_shape)
     # Supply source image as input to the generator and receive the generated image
     gen_img = g_model(src_img)
     # Supply source and generated image as inputs to the discriminator
@@ -31,5 +26,10 @@ def define_gan(g_model, d_model, inp_shape):
     # Instantiate the model with source image as input, fake image generated
     # by the generator and discriminator output (0 or 1) as as outputs
     gan_model = Model(inputs=src_img, outputs=[gen_img, dis_output])
+
+    # Compile the gan model
+    gan_model.compile(loss=["binary_crossentropy", "mae"],
+                      optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5, beta_2=0.999),
+                      loss_weights=[1, 100]) # Authors suggested weighting BCE vs L1 as 1:100
 
     return gan_model
