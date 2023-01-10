d8b34fcd8e297d706d6b92c952f667322c846054
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2023-01-10 22:51:28 +0530

    Update and rename inference_utilities.py to inference.py

--- Files/inference.py
+++ Files/inference.py
@@ -3,8 +3,17 @@ import numpy as np
 import os
 from keras.utils import load_img, img_to_array
 import matplotlib.pyplot as plt 
+import tensorflow as tf
+
+from generator_model import define_generator
 
 
+# Set up path
+VAL_IMAGES_DIRPATH = os.path.join("maps", "val")
+CHECKPOINT_FILEPATH = "generator_checkpoint\checkpoint.ckpt"
+
+
+# Utilities for Inference
 def get_test_sample(dirpath, img_size=(256, 512)):
     # Get file path
     fname = np.random.choice(os.listdir(dirpath))
@@ -37,3 +46,19 @@ def plot_image(src_img, gen_img, tar_img):
         plt.title(titles[i])
         plt.axis(False)
     plt.show()
+
+
+# Get the generator (without weights)
+generator = define_generator()
+# Load in the checkpoint
+generator.load_weights(CHECKPOINT_FILEPATH)
+
+
+# Get the source and target image
+src, tar = get_test_sample(VAL_IMAGES_DIRPATH)
+
+# Get the generated (fake) image
+gen_img = np.squeeze(generator.predict(np.expand_dims(src, axis=0), verbose=0), axis=0)
+
+# Plot the source, generated and target image
+plot_image(src, gen_img, tar)
